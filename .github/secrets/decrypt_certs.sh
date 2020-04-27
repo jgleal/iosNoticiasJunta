6a756d1ea61a5831b8c30b398db1ec0a7983e0d2
Javier <jgleal@gmail.com>
2020-04-27 13:20:50 +0200

    fix env variable

--- .github/secrets/decrypt_certs.sh
+++ .github/secrets/decrypt_certs.sh
@@ -8,7 +8,7 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/"$UUID_PROVISION".mobileprovision
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_CERTIFICATE_ENCRYPT" -A
+security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_CERTIFICATE_PASS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
